eca56a758d0c0b7520245c684bc50e7067c13b92
Lynette <604020768@qq.com>
2020-07-29 16:34:19 +0800

    feat(hook): overrideProps test

--- src/hook/index.spec.ts
+++ src/hook/index.spec.ts
@@ -2,9 +2,11 @@ import "./index";
 import { HOOK_NAME } from "../shared/constants";
 import { getBrickByUid } from "./traverse";
 import { inspectElement } from "./inspector";
+import { overrideProps } from "./overrideProps";
 
 jest.mock("./traverse");
 jest.mock("./inspector");
+jest.mock("./overrideProps");
 
 (getBrickByUid as jest.Mock).mockImplementation((uid) => ({ uid }));
 
@@ -13,6 +15,9 @@ describe("hook", () => {
     const hook = (window as any)[HOOK_NAME];
     hook.inspectBrick(1);
     expect(inspectElement).toBeCalledWith({ uid: 1 });
+
+    hook.overrideProps(1, "propName", "propValue");
+    expect(overrideProps).toBeCalledWith({ uid: 1 }, "propName", "propValue");
 
     expect(hook.pageHasBricks).toBe(false);
     (window as any).BRICK_NEXT_VERSIONS = {};
